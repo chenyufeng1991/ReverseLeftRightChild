dbf9c0fdd725b8b6591f1c9ad32003bc26ef8b79
陈宇峰 <yufengcode@gmail.com>
2016-10-01 10:16:19 +0800

    init

--- ReverseLeftRightChild/main.cpp
+++ ReverseLeftRightChild/main.cpp
@@ -8,8 +8,133 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+typedef struct BTNode{
+    int data;
+    struct BTNode *lChild;
+    struct BTNode *rChild;
+}BiTNode;
+
+//先序创建二叉树
+int CreateBiTree(BiTNode **T)
+{
+    int ch;
+    cin >> ch;
+    if (ch == -1)
+    {
+        *T = NULL;
+        return 0;
+    }
+    else
+    {
+        *T = new BiTNode;
+        if (T == NULL)
+        {
+            cout << "failed" << endl;
+            return 0;
+        }
+        else
+        {
+            (*T)->data = ch;
+            cout << "输入" << ch << "的左子节点:";
+            CreateBiTree(&((*T)->lChild));
+            cout << "输入" << ch << "的右子节点:";
+            CreateBiTree((&(*T)->rChild));
+        }
+    }
+
+    return 1;
+}
+
+//先序遍历二叉树
+void PreOrderBiTree(BiTNode *T)
+{
+    if (T == NULL)
+    {
+        return;
+    }
+    else
+    {
+        cout << T->data << " ";
+        PreOrderBiTree(T->lChild);
+        PreOrderBiTree(T->rChild);
+    }
+}
+
+//中序遍历二叉树
+void MiddleOrderBiTree(BiTNode *T)
+{
+    if (T == NULL)
+    {
+        return;
+    }
+    else
+    {
+        MiddleOrderBiTree(T->lChild);
+        cout << T->data << " ";
+        MiddleOrderBiTree(T->rChild);
+    }
+}
+
+void ReverseLeftRightChild(BiTNode **T)
+{
+    if (*T == NULL || ((*T)->lChild == NULL && (*T)->rChild == NULL))
+    {
+        return;
+    }
+
+    if ((*T)->lChild != NULL && (*T)->rChild != NULL)
+    {
+        swap((*T)->lChild, (*T)->rChild);
+
+        ReverseLeftRightChild(&((*T)->lChild));
+        ReverseLeftRightChild(&((*T)->rChild));
+    }
+
+    if ((*T)->lChild != NULL && (*T)->rChild == NULL)
+    {
+        (*T)->rChild  = (*T)->lChild;
+        ReverseLeftRightChild(&((*T)->rChild));
+    }
+
+    if ((*T)->lChild == NULL && (*T)->rChild != NULL)
+    {
+        (*T)->lChild  = (*T)->rChild;
+        ReverseLeftRightChild(&((*T)->lChild));
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    BiTNode *T;
+    cout << "请输入第一个节点的值，-1表示没有叶节点：" << endl;
+    CreateBiTree(&T);
+
+    cout << "先序遍历二叉树：";
+    PreOrderBiTree(T);
+    cout << endl;
+
+    ReverseLeftRightChild(&T);
+    cout << "交换左右子树后,先序遍历二叉树：";
+    PreOrderBiTree(T);
+    cout << endl;
+
+    cout << "交换左右子树后,,中序遍历二叉树：";
+    MiddleOrderBiTree(T);
+    cout << endl;
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
